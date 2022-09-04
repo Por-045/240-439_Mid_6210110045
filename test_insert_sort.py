7ab913be98b5e161bee35924b6f4a34c64fb8ed1
Por-045 <6210110045@psu.ac.th>
2022-09-04 14:25:07 +0700

    minus_test

--- test_insert_sort.py
+++ test_insert_sort.py
@@ -10,3 +10,11 @@ class InsertSortTest(unittest.TestCase):
         result = insertion_sort(number)
 
         self.assertEqual(result, excepted_result)
+
+    def test_give_156_54_minus20_65_minus532_should_minus532_minus20_54_65_156(self):
+        number = [156, 54, -20, 65, -532]
+        excepted_result = [-532, -20, 54, 65, 156]
+
+        result = insertion_sort(number)
+
+        self.assertEqual(result, excepted_result)
